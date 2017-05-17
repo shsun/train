73223392e67ad9c1d9a19d9fca8d4f2ee74285d7
shsun <shsun@shsuns-MacBook-Pro.local>
2017-05-17 16:59:49 +0800

    a

---
+++ src/main/java/base/redis/RedisCacheTransfer.java
@@ -0,0 +1,16 @@
+package base.redis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.connection.jedis.JedisConnectionFactory;
+
+/**
+ * 
+ * @author shsun
+ *
+ */
+public class RedisCacheTransfer {
+	@Autowired
+	public void setJedisConnectionFactory(JedisConnectionFactory jedisConnectionFactory) {
+		RedisCache.setJedisConnectionFactory(jedisConnectionFactory);
+	}
+}
