8f92e2e1a121b8e7499e3d4d5438077a981f7ec0
shsun <shsun@shsuns-MacBook-Pro.local>
2017-07-11 14:55:51 +0800

    init

---
+++ src/main/java/com/demo/basic/XBasicService.java
@@ -0,0 +1,142 @@
+package com.demo.services;
+
+import com.baomidou.mybatisplus.plugins.Page;
+import com.demo.basic.ISumEntry;
+import com.demo.user.UserEntry;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * @author shsun
+ */
+public abstract class XBasicService<PARAMETER, RESULT> {
+
+    @SuppressWarnings("unchecked")
+    public Map<String, Object> retrieveViewData(HttpServletRequest req, HttpServletResponse res) {
+        Map<String, Object> data = new HashMap<String, Object>();
+        try {
+            String contextPath = req.getContextPath();
+            data.put("contextPath", contextPath);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return data;
+    }
+
+    /**
+     * @param req
+     * @param res
+     * @param u
+     * @param p
+     * @throws Exception
+     */
+    public abstract void create(HttpServletRequest req, HttpServletResponse res, UserEntry u, PARAMETER p) throws Exception;
+
+    /**
+     * @param req
+     * @param res
+     * @param u
+     * @param p
+     * @return
+     * @throws Exception
+     */
+    public abstract RESULT retrieveByPK(HttpServletRequest req, HttpServletResponse res, UserEntry u, PARAMETER p) throws Exception;
+
+    /**
+     * @param req
+     * @param res
+     * @param u
+     * @param p
+     * @return
+     * @throws Exception
+     */
+    public Page retrievePage(HttpServletRequest req, HttpServletResponse res, UserEntry u, PARAMETER p) throws Exception {
+        Page<RESULT> page = new Page<RESULT>();
+        List<RESULT> records = retrieveList(req, res, u, p);
+        page.setRecords(records);
+        return page;
+    }
+
+    /**
+     * @param req
+     * @param res
+     * @param u
+     * @param p
+     * @return
+     * @throws Exception
+     */
+    public abstract List<RESULT> retrieveList(HttpServletRequest req, HttpServletResponse res, UserEntry u, PARAMETER p) throws Exception;
+
+    /**
+     * @param req
+     * @param res
+     * @param u
+     * @param p
+     * @return
+     * @throws Exception
+     */
+    public RESULT updateAndGet(HttpServletRequest req, HttpServletResponse res, UserEntry u, PARAMETER p) throws Exception {
+        this.update(req, res, u, p);
+        RESULT result = this.retrieveByPK(req, res, u, p);
+        return result;
+    }
+
+    /**
+     * @param req
+     * @param res
+     * @param u
+     * @param p
+     * @return
+     * @throws Exception
+     */
+    public abstract void update(HttpServletRequest req, HttpServletResponse res, UserEntry u, PARAMETER p) throws Exception;
+
+    /**
+     *
+     * @param req
+     * @param res
+     * @param u
+     * @param p
+     * @throws Exception
+     */
+    public RESULT deleteAndGet(HttpServletRequest req, HttpServletResponse res, UserEntry u, PARAMETER p) throws Exception {
+        RESULT result = this.retrieveByPK(req, res, u, p);
+        this.delete(req, res, u, p);
+        return result;
+    }
+
+    /**
+     * @param req
+     * @param res
+     * @param u
+     * @param p
+     * @throws Exception
+     */
+    public abstract void delete(HttpServletRequest req, HttpServletResponse res, UserEntry u, PARAMETER p) throws Exception;
+
+    /**
+     * @param req
+     * @param res
+     * @param u
+     * @param p
+     * @return
+     * @throws Exception
+     */
+    public abstract int sumOne(HttpServletRequest req, HttpServletResponse res, UserEntry u, PARAMETER p) throws Exception;
+
+    /**
+     * 
+     * @param req
+     * @param res
+     * @param u
+     * @param p
+     * @return
+     * @throws Exception
+     */
+    public abstract ISumEntry sumMultiple(HttpServletRequest req, HttpServletResponse res, UserEntry u, PARAMETER p) throws Exception;
+}
